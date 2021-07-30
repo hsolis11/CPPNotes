59c3a6b43edc4e6fff5dc3c0f257e19716835f41
Hector Solis <68400979+hsolis11@users.noreply.github.com>
2021-07-29 21:50:38 -0500

    Update graph.cpp

--- final_project2/graph.cpp
+++ final_project2/graph.cpp
@@ -1,282 +1,121 @@
-#include "graph.h"
+#include "edgelist.h"
 
+EdgeList::EdgeList(){
+    edge = NULL;
 
-Graph::Graph(){
-    vertex = NULL;
-    count = 0;
 }
 
-Graph::~Graph(){
-    // clear();
-}
-
-bool Graph::isEmpty(){
-    return count < 0;
-}
-
-int Graph::vertices(){
-    return count;
-}
-
-bool Graph::addEdge(Vertex vertexA, Vertex vertexB, int weight){
-
-    // check if VertexA.label or VertexB.label exists already
-    // if(vertexA.label && vertexB.label do not exist){
-    //     add both or missing vertex then add the edge
-    // }
-}
-
-
-/*****************************************
- * Name: Hector Solis
- * Coding 05
- * Purpose: Create a fully functional doubly
- * linked list class
- *****************************************/
-
-#include "linkedlist.h"
-
-LinkedList::LinkedList(){
-    head = NULL;
-}
-
-LinkedList::~LinkedList(){
+EdgeList::~EdgeList(){
     clearList();
 }
 
-bool LinkedList::addVertex(int id, string* info){
-    
+bool EdgeList::addEdge(Vertex* newVertex, int weight){
     bool success = false;
-    bool valid = id > 0 && *info != "";
-    bool done = false;
+    bool issues = false;
+    Edge *current = edge;
 
-    if(!head && valid && !done){
-        success = newVertex(id, info);
-        done = success;
+    if(!edge){
+        // If there are no edges. Add a new edge.
+        Edge *newEdge = new Edge;
+        newEdge->nextEdge = NULL;
+        newEdge->weight = weight;
+        newEdge->vertex = newVertex;
+        edge = newEdge;
+        success = true;
     }
-    if (id < head->data.id && valid && !done){
-        success = newHead(id, info);
-        done = success;
+    while(current && !issues){
+        if (current && current->vertex->data.id == newVertex->data.id){
+            issues = true;
+        } else if(!success && !current->nextEdge){
+            // if it is tail. Then add a new Edge
+            Edge *newEdge = new Edge;
+            newEdge->nextEdge = NULL;
+            newEdge->weight = weight;
+            newEdge->vertex = newVertex;
+            current->nextEdge = newEdge;
+            success = true;
+        } else {
+            // else keep going through nextEdge
+            current = current->nextEdge;
+        }
     }
-    if (id > head->data.id && valid && !done){
-        success = insertNewVertex(id, info);
-    }
-    
+
     return success;
 }
 
-bool LinkedList::deleteVertex(int id){
-    
-    Vertex *current = head;
+bool EdgeList::deleteEdge(int id){
+    Edge *current = new Edge;
+    current = edge;
     bool success = false;
 
     while(current && !success){
-        if(current->data.id == id && !current->prev){
-            success = deleteHead(current);
+        // cout << current->vertex->data.id;
 
-        } else if(current->data.id == id && !current->next){
-            success = deleteTail(current);
-
-        } else if(current->data.id == id){
-            success = deleteMiddle(current);
-
-        } else {
-            current = current->next;
-        }
-    }
-
-    return success;
-}
-
-bool LinkedList::getVertex(int id, Data* data){
-    
-    Vertex *current = head;
-    data->id = -1;
-    data->data = "";
-    bool success = false;
-    bool done = false;
-    
-    while(current && !done){
-        if(current->data.id == id){
-            data->id = current->data.id;
-            data->data = current->data.data;
-            success = true;
-            done = true;
-        } else {
-            current = current->next;
-        }
-    }
-
-    return success;
-}
-
-void LinkedList::printList(bool backwards){
-    
-    Vertex *current = head;
-
-    cout << "printing list..." << endl;
-
-    if(!current){
-        cout << "\tThere are 0 Vertexs. " << endl;
-    } else {
-        if(backwards){
-            bool tail = false;
-            while(current && !tail){
-                if(current->next){
-                    current = current->next;
-                } else {
-                    tail = true;
-                }
+        if(edge->vertex->data.id == id){
+            // if its head being deleted
+            
+            if(!edge->nextEdge){
+                delete current;
+                edge = NULL;
+                success = true;
+            } else {
+                edge = current->nextEdge;
+                delete current;
+                success = true;
+            }
+            
+        } 
+        if(current->nextEdge){
+            if (current->nextEdge->vertex->data.id == id && !current->nextEdge->nextEdge){
+                // if its tail being deleted. Checking one step ahead
+                delete current->nextEdge;
+                current->nextEdge = NULL;
+                success = true;
+            } else if (current->nextEdge->vertex->data.id == id){
+                // if its anything else being deleted
+                Edge *temp = new Edge;
+                temp = current->nextEdge->nextEdge;
+                delete current->nextEdge;
+                current->nextEdge = temp;
+                success = true;
             }
         }
-        while(current){
-            cout << "\t" << current->data.id << ": " << current->data.data << endl;
-            current = backwards? current->prev: current->next;
-        }
-    } 
-}
-
-int LinkedList::getCount(){
-    
-    int count = 0;
-
-    Vertex *current = head;
-    while(current){
-        current = current->next;
-        count++;
+        current = current->nextEdge;
     }
-    return count;
-}
 
-bool LinkedList::clearList(){
-    
-    bool success = false;
-    Vertex *current = head;
-    bool done = false;
-
-    while(current){
-        Vertex *temp = current;
-        current = current->next;
-        delete temp;
-    }
-    if(current == NULL){
-        head = NULL;
-        success = true;
-    }
     return success;
 }
 
-bool LinkedList::exists(int id){
-    
+bool EdgeList::exists(int id){
     bool exists = false;
     bool done = false;
 
-    Vertex *current = head;
+    Edge *current = edge;
     while(current && !done){
-        if(current->data.id == id){
+        if(current->vertex->data.id == id){
             exists = true;
             done = true;
-        } 
-        current = current->next;
+        }
+        current = current->nextEdge;
     }
+
     return exists;
 }
 
-/************************
-* Private methods
-* *********************/
-
-
-void LinkedList::fillData(Vertex* new_Vertex, int id, string* info){
-    new_Vertex->data.id = id;
-    new_Vertex->data.data = *info;
-}
-
-bool LinkedList::newVertex(int id, string* info){
-    Vertex *new_Vertex = new Vertex;
-    fillData(new_Vertex, id, info);
-
-    head = new_Vertex;
-    head->prev = NULL;
-    head->next = NULL;
-    return true;
-}
-
-bool LinkedList::newHead(int id, string* info){
-    Vertex *new_Vertex = new Vertex;
-    fillData(new_Vertex, id, info);
-
-    head->prev = new_Vertex;
-    new_Vertex->next = head;
-    new_Vertex->prev = NULL;
-    head = new_Vertex;
-    return true;
-}
-
-void LinkedList::newTail(Vertex* new_Vertex, Vertex* current){
-    new_Vertex->next = NULL;
-    new_Vertex->prev = current;
-    current->next = new_Vertex;
-}
-
-void LinkedList::newMiddle(Vertex* new_Vertex, Vertex* current){
-    new_Vertex->next = current;
-    new_Vertex->prev = current->prev;
-    current->prev->next = new_Vertex;
-    current->prev = new_Vertex;
-}
-
-bool LinkedList::insertNewVertex(int id, string* info){
-    Vertex *current = head;
-    bool issues = false;
+bool EdgeList::clearList(){
     bool success = false;
-        
-    while(current && !issues && !success){
-        if(id == current->data.id){
-            issues = true;
-            success = false;
-        } 
-        if(id > current->data.id && !current->next && !issues){
-            issues = true;
-            Vertex *new_Vertex = new Vertex;
-            fillData(new_Vertex, id, info);
-            newTail(new_Vertex, current);
-            success = true;
-        }
-        if(id > current->data.id  && !issues){
-            current = current->next;            
-        } else if(!issues){
-            Vertex *new_Vertex = new Vertex;
-            fillData(new_Vertex, id, info);
-            newMiddle(new_Vertex, current);
-            success = true;
-        }
+    Edge *current = edge;
+    bool done = false;
+
+    while(current){
+        Edge *temp = current;
+        current = current->nextEdge;
+        delete temp;
     }
-    return success; 
-}
-
-bool LinkedList::deleteHead(Vertex* current){
-    if(!current->next && !current->prev){
-        delete current;
-        head = NULL;
-    } else {
-        current->next->prev = NULL;
-        head = current->next;
-        delete current;
+    if(current == NULL){
+        edge = NULL;
+        success = true;
     }
-    
-    return true;
-}
 
-bool LinkedList::deleteTail(Vertex* current){
-    current->prev->next = NULL;
-    delete current;
-    return true;
-}
-
-bool LinkedList::deleteMiddle(Vertex* current){
-    current->prev->next = current->next;
-    current->next->prev = current->prev;
-    delete current;
-    return true;
+    return success;
 }
